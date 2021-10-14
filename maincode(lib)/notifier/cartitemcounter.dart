cda0326adb19da73663727ae013ffdcb16069fe8
Vaibhavchopra1 <70208602+Vaibhavchopra1@users.noreply.github.com>
2021-10-14 09:40:35 +0530

    Add files via upload

---
+++ maincode(lib)/notifier/cartitemcounter.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:craft/Config/config.dart';
+
+class CartItemCounter extends ChangeNotifier{
+  int _counter=EcommerceApp.sharedPreferences.getStringList(EcommerceApp.userCartList).length-1;
+  int get count => _counter;
+
+  Future<void> displayResult() async {
+
+    //_counter++;
+    print(EcommerceApp.sharedPreferences.getStringList(EcommerceApp.userCartList).length);
+    _counter=EcommerceApp.sharedPreferences.getStringList(EcommerceApp.userCartList).length-1;
+    await Future.delayed(const Duration(milliseconds: 100), (){
+      notifyListeners();
+    });
+
+  }
+
+}
