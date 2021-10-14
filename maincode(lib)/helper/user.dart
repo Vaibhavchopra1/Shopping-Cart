32a9625a02a512f341c4dfc84c88b1c88e00fd74
Vaibhavchopra1 <70208602+Vaibhavchopra1@users.noreply.github.com>
2021-10-14 09:34:38 +0530

    Add files via upload

---
+++ maincode(lib)/helper/user.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:craft/models/cart_item.dart';
+import 'package:craft/models/user.dart';
+
+class UserServices{
+  String collection = "users";
+  Firestore _firestore = Firestore.instance;
+
+  void createUser(Map<String, dynamic> values) {
+    String id = values["id"];
+    _firestore.collection(collection).document(id).setData(values);
+  }
+
+  void updateUserData(Map<String, dynamic> values){
+    _firestore.collection(collection).document(values['id']).updateData(values);
+  }
+
+
+  void addToCart({String userId, CartItemModel cartItem}){
+    print("THE USER ID IS: $userId");
+    print("cart items are: ${cartItem.toString()}");
+    _firestore.collection(collection).document(userId).updateData({
+      "cart": FieldValue.arrayUnion([cartItem.toMap()])
+    });
+  }
+
+  void removeFromCart({String userId, CartItemModel cartItem}){
+    print("THE USER ID IS: $userId");
+    print("cart items are: ${cartItem.toString()}");
+    _firestore.collection(collection).document(userId).updateData({
+      "cart": FieldValue.arrayRemove([cartItem.toMap()])
+    });
+  }
+
+  Future<UserModel> getUserById(String id) => _firestore.collection(collection).document(id).get().then((doc){
+    return UserModel.fromSnapshot(doc);
+  });
+}
